8f53f2b324237eac0e1b391f401e4a97c5f1ab8a
giovanabarth <148559217+giovanabarth@users.noreply.github.com>
2025-04-26 10:42:26 -0300

    commit initial: estrutura do projeto com codegen

---
+++ DividiAqui/src/main/java/com/giovana/DividiAqui/DividiAquiApplication.java
@@ -0,0 +1,13 @@
+package com.giovana.DividiAqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividiAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividiAquiApplication.class, args);
+	}
+
+}
---
+++ DividiAqui/src/test/java/com/giovana/DividiAqui/DividiAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovana.DividiAqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividiAquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
